10a555ad49acf666b9c40049434ea30389ab104f
Artem <artem.lapa@gmail.com>
2021-05-27 16:52:05 +0300

    :tada: initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const tasks = [
+  { title: "do somthing" },
+  { title: "check smartphone" },
+  { title: "clear shoose" },
+];
+
+const App = () => {
+  return (
+    <>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  );
+};
+
+export default App;
