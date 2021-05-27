f25e319d4e7e3015c33a46ebae799a25284a1dd5
Artem <artem.lapa@gmail.com>
2021-05-27 17:29:51 +0300

    :sparkles: add first changes for App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-const tasks = [
-  { title: "do somthing" },
-  { title: "check smartphone" },
-  { title: "clear shoose" },
+let initTasks = [
+  { id: 1, title: "do somthing", done: true },
+  { id: 2, title: "check smartphone", done: false },
+  { id: 3, title: "clear shoose", done: true },
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(initTasks);
+
+  const changeTaskStatus = (tasks, el) => {
+    console.log(el);
+    setTasks(
+      tasks.map((item) =>
+        item.id === el.id ? { ...item, done: !item.done } : item
+      )
+    );
+  };
+
   return (
     <>
-      <ul>
-        <li></li>
+      <ul className="tasks-list">
+        {tasks.map((el) => (
+          <li
+            key={el.id}
+            className={el.done ? "task-done" : ""}
+            onClick={() => changeTaskStatus(tasks, el)}
+          >
+            {el.title}
+          </li>
+        ))}
       </ul>
     </>
   );
