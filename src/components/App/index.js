2cf58f195c5b736a2f6537dfe7d1d602816c7ab8
Artem <artem.lapa@gmail.com>
2021-06-01 16:07:14 +0300

    :construction: add active tabs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,27 +7,29 @@ import {
   faSquare,
   faTrashAlt,
 } from "@fortawesome/free-regular-svg-icons";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+// import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 let initTasks = [];
 
 const App = () => {
-  const [newTask, setNewTasks] = useState("");
+  // const [newTask, setNewTasks] = useState("");
   const [tasks, setTasks] = useState(initTasks);
   const [activeTab, setActiveTab] = useState("all");
 
   useEffect(() => {
     setTimeout(() => {
       setTasks([
-        { id: 1, title: "do somthing", done: true, delete: true },
-        { id: 2, title: "check smartphone", done: true, delete: false },
-        { id: 3, title: "clear shoose", done: false, delete: false },
+        { id: 1, title: "Wake up", done: true, delete: true },
+        { id: 2, title: "Go to the bath", done: true, delete: false },
+        { id: 3, title: "Make the breakfast", done: false, delete: false },
+        { id: 4, title: "Brush your teeth", done: false, delete: false },
+        { id: 5, title: "Dress your self", done: false, delete: false },
+        { id: 6, title: "Take the laptop", done: false, delete: false },
       ]);
     }, 2000);
   }, []);
 
   const changeTaskStatus = (el) => {
-    // console.log(el);
     setTasks(
       tasks.map((item) =>
         item.id === el.id ? { ...item, done: !item.done } : item
@@ -35,14 +37,22 @@ const App = () => {
     );
   };
 
-  const enterNewTask = (e) => {
-    setNewTasks(e.target.value);
+  const changeTaskStatusDelete = (el) => {
+    setTasks(
+      tasks.map((item) =>
+        item.id === el.id ? { ...item, delete: !item.delete } : item
+      )
+    );
   };
 
-  const addNewTask = () => {
-    setTasks([...tasks, { id: tasks.length + 1, title: newTask, done: false }]);
-    setNewTasks("");
-  };
+  // const enterNewTask = (e) => {
+  //   setNewTasks(e.target.value);
+  // };
+
+  // const addNewTask = () => {
+  //   setTasks([...tasks, { id: tasks.length + 1, title: newTask, done: false }]);
+  //   setNewTasks("");
+  // };
 
   return (
     <div className="device__wrapper">
@@ -54,61 +64,235 @@ const App = () => {
         </div>
 
         <div className="content">
-          <div className="tasks all__tasks active">
-            <div className="tasks__tab__wrapper">
+          <div
+            className={`tasks all__tasks ${
+              activeTab === "all" ? "active" : ""
+            }`}
+          >
+            <div
+              className="tasks__tab__wrapper"
+              onClick={() => {
+                setActiveTab("all");
+              }}
+            >
               <div className="title__wrapper">
                 <div className="title">All tasks</div>
                 <div className="description">Here you can manage all tasks</div>
               </div>
-              <div className="task__counter">12</div>
+              <div className="task__counter">{tasks.length}</div>
             </div>
-
-            <div className="tasks__list__wrapper show">
+            <div
+              className={`tasks__list__wrapper ${
+                activeTab === "all" ? "show" : ""
+              }`}
+            >
               <ul className="tasks__list">
-                {tasks.map((el) => (
-                  <li className="task__element" key={el.id}>
-                    <FontAwesomeIcon icon={faSquare} />
-                    <span className="task__text">{el.title}</span>
-                    <FontAwesomeIcon icon={faTrashAlt} />
-                  </li>
-                ))}
+                {tasks.map((el) => {
+                  const cssClass = el.delete
+                    ? "removed"
+                    : el.done
+                    ? "done"
+                    : "";
+                  return (
+                    <li className={`task__element ${cssClass}`} key={el.id}>
+                      <FontAwesomeIcon
+                        icon={el.done ? faCheckSquare : faSquare}
+                        className="fa"
+                        onClick={() => changeTaskStatus(el)}
+                      />
+                      <span className="task__text">{el.title}</span>
+                      <FontAwesomeIcon
+                        icon={faTrashAlt}
+                        className="fa"
+                        onClick={() => changeTaskStatusDelete(el)}
+                      />
+                    </li>
+                  );
+                })}
               </ul>
             </div>
           </div>
 
-          <div className="tasks todo__tasks">
-            <div className="tasks__tab__wrapper">
+          <div
+            className={`tasks todo__tasks ${
+              activeTab === "todo" ? "active" : ""
+            }`}
+          >
+            <div
+              className="tasks__tab__wrapper"
+              onClick={() => {
+                setActiveTab("todo");
+              }}
+            >
               <div className="title__wrapper">
                 <div className="title">Todo tasks</div>
                 <div className="description">
                   Here you can manage tasks to be completed
                 </div>
               </div>
-              <div className="task__counter">10</div>
+              <div className="task__counter">
+                {
+                  tasks.filter((el) => {
+                    return !el.done && !el.delete;
+                  }).length
+                }
+              </div>
+            </div>
+            <div
+              className={`tasks__list__wrapper ${
+                activeTab === "todo" ? "show" : ""
+              }`}
+            >
+              <ul className="tasks__list">
+                {tasks
+                  .filter((el) => {
+                    return !el.done && !el.delete;
+                  })
+                  .map((el) => {
+                    const cssClass = el.delete
+                      ? "removed"
+                      : el.done
+                      ? "done"
+                      : "";
+                    return (
+                      <li className={`task__element ${cssClass}`} key={el.id}>
+                        <FontAwesomeIcon
+                          icon={el.done ? faCheckSquare : faSquare}
+                          className="fa"
+                          onClick={() => changeTaskStatus(el)}
+                        />
+                        <span className="task__text">{el.title}</span>
+                        <FontAwesomeIcon
+                          icon={faTrashAlt}
+                          className="fa"
+                          onClick={() => changeTaskStatusDelete(el)}
+                        />
+                      </li>
+                    );
+                  })}
+              </ul>
             </div>
           </div>
 
-          <div className="tasks done__tasks">
-            <div className="tasks__tab__wrapper">
+          <div
+            className={`tasks done__tasks ${
+              activeTab === "done" ? "active" : ""
+            }`}
+          >
+            <div
+              className="tasks__tab__wrapper"
+              onClick={() => {
+                setActiveTab("done");
+              }}
+            >
               <div className="title__wrapper">
                 <div className="title">Done tasks</div>
                 <div className="description">
                   Here you manage the tasks that you have already done
                 </div>
               </div>
-              <div className="task__counter">1</div>
+              <div className="task__counter">
+                {
+                  tasks.filter((el) => {
+                    return el.done && !el.delete;
+                  }).length
+                }
+              </div>
+            </div>
+            <div
+              className={`tasks__list__wrapper ${
+                activeTab === "done" ? "show" : ""
+              }`}
+            >
+              <ul className="tasks__list">
+                {tasks
+                  .filter((el) => {
+                    return el.done && !el.delete;
+                  })
+                  .map((el) => {
+                    const cssClass = el.delete
+                      ? "removed"
+                      : el.done
+                      ? "done"
+                      : "";
+                    return (
+                      <li className={`task__element ${cssClass}`} key={el.id}>
+                        <FontAwesomeIcon
+                          icon={el.done ? faCheckSquare : faSquare}
+                          className="fa"
+                          onClick={() => changeTaskStatus(el)}
+                        />
+                        <span className="task__text">{el.title}</span>
+                        <FontAwesomeIcon
+                          icon={faTrashAlt}
+                          className="fa"
+                          onClick={() => changeTaskStatusDelete(el)}
+                        />
+                      </li>
+                    );
+                  })}
+              </ul>
             </div>
           </div>
 
-          <div className="tasks done__tasks">
-            <div className="tasks__tab__wrapper">
+          <div
+            className={`tasks removed__tasks ${
+              activeTab === "removed" ? "active" : ""
+            }`}
+          >
+            <div
+              className="tasks__tab__wrapper"
+              onClick={() => {
+                setActiveTab("removed");
+              }}
+            >
               <div className="title__wrapper">
                 <div className="title">Removed tasks</div>
                 <div className="description">
                   Here you can see the tasks that you have been removed
                 </div>
               </div>
-              <div className="task__counter">1</div>
+              <div className="task__counter">
+                {
+                  tasks.filter((el) => {
+                    return el.delete;
+                  }).length
+                }
+              </div>
+            </div>
+            <div
+              className={`tasks__list__wrapper ${
+                activeTab === "removed" ? "show" : ""
+              }`}
+            >
+              <ul className="tasks__list">
+                {tasks
+                  .filter((el) => {
+                    return el.delete;
+                  })
+                  .map((el) => {
+                    const cssClass = el.delete
+                      ? "removed"
+                      : el.done
+                      ? "done"
+                      : "";
+                    return (
+                      <li className={`task__element ${cssClass}`} key={el.id}>
+                        <FontAwesomeIcon
+                          icon={el.done ? faCheckSquare : faSquare}
+                          className="fa"
+                          onClick={() => changeTaskStatus(el)}
+                        />
+                        <span className="task__text">{el.title}</span>
+                        <FontAwesomeIcon
+                          icon={faTrashAlt}
+                          className="fa"
+                          onClick={() => changeTaskStatusDelete(el)}
+                        />
+                      </li>
+                    );
+                  })}
+              </ul>
             </div>
           </div>
         </div>
