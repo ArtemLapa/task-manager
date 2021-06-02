c57416f07bf704a7282cca01a1a1d02a53b856d2
Artem <artem.lapa@gmail.com>
2021-06-02 17:54:13 +0300

    :lipstick: add styled-components

--- src/components/FooterComponent/index.js
+++ src/components/FooterComponent/index.js
@@ -1,4 +1,78 @@
 import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.div`
+  position: sticky;
+  bottom: 0;
+  width: 100%;
+  .add__button__wrapper {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    right: 0px;
+    bottom: 0px;
+    transform: translate(-20%, -20%);
+    background-color: ${(props) => props.addButtonWrapperBgColor};
+    border-radius: 50%;
+    cursor: pointer;
+    opacity: 0.7;
+    transition: transform 0.3s;
+    &:hover {
+      transform: translate(-20%, -20%) scale(1.035);
+      opacity: 1;
+    }
+
+    .hr {
+      position: absolute;
+      width: 30px;
+      height: 3px;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      background-color: ${(props) => props.hrBgColor};
+      transition: transform 0.3s ease;
+
+      &.vr {
+        transform: translate(-50%, -50%) rotate(90deg);
+      }
+    }
+  }
+
+  .add__task__form__wrapper {
+    height: 0px;
+    background-color: ${(props) => props.addTaskFormWrapperBgColor};
+    padding: 0px 20px;
+    border-top-left-radius: 25px;
+    border-top-right-radius: 25px;
+    box-shadow: 0px -4px 6px 0px rgba(0, 0, 0, 0.125);
+    display: flex;
+    flex-direction: column;
+    transition: height 0.3s ease;
+    overflow: hidden;
+
+    label {
+      font-weight: 700;
+      margin: 20px 0;
+    }
+
+    input {
+      border: unset;
+      border-bottom: 1px solid ${(props) => props.inputUnderLineColor};
+      height: 20px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+  &.active {
+    .add__task__form__wrapper {
+      height: 166px;
+      transition: all 0.3s ease-in;
+    }
+  }
+`;
 
 const FooterComponent = ({
   isAddTaskFormOpen,
@@ -8,7 +82,13 @@ const FooterComponent = ({
   keyBoardHandler,
 }) => {
   return (
-    <div className={`footer ${isAddTaskFormOpen ? "active" : ""}`}>
+    <Footer
+      className={isAddTaskFormOpen ? "active" : ""}
+      addButtonWrapperBgColor="#000"
+      addTaskFormWrapperBgColor="#fff"
+      hrBgColor="#fff"
+      inputUnderLineColor="#ccc"
+    >
       <div
         className="add__button__wrapper"
         onClick={() => {
@@ -29,7 +109,7 @@ const FooterComponent = ({
           onKeyUp={keyBoardHandler}
         />
       </div>
-    </div>
+    </Footer>
   );
 };
 
